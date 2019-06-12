3a4d796722ef2b01a46ddaa26d5061dea618a1a5
Engin Aydogdu <engin_87@msn.com>
2019-06-12 22:46:56 +0300

    Add project files.

---
+++ FormattedMailSender/Mail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace FormattedMailSender
+{
+    public class Mail
+    {
+        public void SendMail(string receiver)
+        {
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                Host = ConfigurationManager.AppSettings["Host"],
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(ConfigurationManager.AppSettings["SMTPuser"], ConfigurationManager.AppSettings["SMTPpassword"]),
+                Port = int.Parse(ConfigurationManager.AppSettings["Port"])
+            };
+
+            using (var message = new MailMessage(ConfigurationManager.AppSettings["SMTPuser"], receiver)
+            {
+                Subject = "Konu",
+                Body = CreateBody(),
+                IsBodyHtml = true
+            })
+            {
+                smtpClient.Send(message);
+            }
+
+                
+        }
+
+        private string CreateBody()
+        {
+            string body = string.Empty;
+            using (StreamReader streamReader = new StreamReader(Path.GetFullPath("MailTemplate.html"))
+            {
+
+            })
+
+            body = streamReader.ReadToEnd();
+
+            body = body.Replace("{firstname}", "Engin");
+            body = body.Replace("{lastname}", "Aydogdu");
+            body = body.Replace("{tckn}", "12345678901");
+
+            return body;
+
+        }
+    }
+}
---
+++ FormattedMailSender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattedMailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mail mail = new Mail();
+            mail.SendMail("engin_87@msn.com");
+
+        }
+    }
+}
